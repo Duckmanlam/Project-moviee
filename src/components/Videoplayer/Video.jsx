6135175240cf8e4972d31c498d5c5adc4dcf9243
khatranas <vankhatran1234@gmail.com>
2023-11-22 17:49:30 +0700

    dd

--- src/components/Videoplayer/Video.jsx
+++ src/components/Videoplayer/Video.jsx
@@ -1,7 +1,7 @@
 import ReactPlayer from 'react-player';
 
 export default function Video() {
-  const videoUrl = 'https://www.youtube.com/watch?v=-tpLfKaYD4g'; // Thay đổi URL video tùy thuộc vào nhu cầu của bạn
+  const videoUrl = 'https://firebasestorage.googleapis.com/v0/b/stream-api-82345.appspot.com/o/webs%2F1700288782.mp4?alt=media'; // Thay đổi URL video tùy thuộc vào nhu cầu của bạn
 
   return (
     <div className='video-wrapper'>
